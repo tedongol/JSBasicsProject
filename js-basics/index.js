063b5d5be04e8a4f9e26e5a261e745dc74dcb3d3
tedongol <teena.dongol777@gmail.com>
2020-09-22 20:18:48 +1000

    js updates

--- js-basics/index.js
+++ js-basics/index.js
@@ -14,4 +14,27 @@ function callThreeTimes(f) {
 function cry() {
   console.log(" I'M SO SAD");
 }
-callThreeTimes(cry);
+//callThreeTimes(cry);
+
+function repeatNtime(action, num) {
+  for (let i = 0; i < num; i++) {
+    action();
+  }
+}
+repeatNtime(cry, 10);
+
+function makeBetween(x, y) {
+  return function (num) {
+    if (num >= x && num <= y) {
+      return true;
+    }
+    return false;
+  };
+}
+const averageWeather = makeBetween(20, 40);
+console.log(averageWeather(50));
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', function () {
+  alert('Go Away!!');
+});
